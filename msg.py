cf78e5ac14786d910dd0b5c2f2902f4b252aaefe
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-12 21:26:59 +0100

    continue

--- msg.py
+++ msg.py
@@ -11,26 +11,28 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import logging
 logging.basicConfig(level = logging.DEBUG, format = '%(levelname)s, %(message)s')
+logging.info('Opening .... ' + os.path.basename(url))
 #logging.disable(logging.CRITICAL)
 
-# Set basic url to open and config login.
+
+# Set basic url to open.
 url = 'http://mail.yahoo.com'
-logging.info('Opening .... ' + os.path.basename(url))
 
-
- # Set variables with login email, password, email of receiver and a message to send using sys module.
+# Check if proper amount of arguments were passed in command line.
 if len(sys.argv) < 2:
     print('Please pass your login, password, receiver\'s email and a message to send.')
 elif len(sys.argv) < 4:
     print('You forgot to pass your message.')
 
-# TODO: Open url, enter login and password using selenium module.
-browser = webdriver.Firefox()
-browser.get(url)
-
+# Set variables with login email, password, email of receiver and a message to send using sys module.
 login = sys.argv[1]
 password = sys.argv[2]
 receiver_email = sys.argv[3]
 message = ' '.join(sys.argv[4:])
+
+# TODO: Open url, enter login and password using selenium module.
+browser = webdriver.Firefox()
+browser.get(url)
+
 
 # TODO: Send message using selenium.
